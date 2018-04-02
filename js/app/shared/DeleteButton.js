5d8e24380e04a616243b5fe00ae9a3a3a3c41b18
Dani Kenan <danikenan@gmail.com>
2018-04-02 03:32:55 +0300

    improved remove btn look and behavior

--- js/app/shared/DeleteButton.js
+++ js/app/shared/DeleteButton.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { Alert } from 'react-native'
 import styled from 'styled-components/native'
-import { Ionicons } from '@expo/vector-icons'
+import { Entypo } from '@expo/vector-icons'
 
 const Button = styled.TouchableOpacity``
 
-const DeleteButton = ({style, onPress}) => (
-  <Button style={style} onPress={onPress}>
-    <Ionicons name="ios-remove-circle" size={30} color="red"/>
+const confirmed = onPress => () =>
+  Alert.alert(
+    'Remove',
+    'Are you sure you want to remove this item?',
+    [
+      { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+      { text: 'Yes', onPress: () => onPress() },
+    ],
+    { cancelable: false },
+  )
+
+const DeleteButton = ({ style, onPress }) => (
+  <Button style={style} onPress={confirmed(onPress)}>
+    <Entypo name='cross' size={30} color='#DF8244' />
   </Button>
 )
 
