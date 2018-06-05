01cb4798ce0dee3b568bcf6f9574eba1951b97b6
yoave11 <yoave11@yahoo.com>
2018-06-05 20:01:19 +0300

    change to new auth account

--- js/app/config.js
+++ js/app/config.js
@@ -1,5 +1,5 @@
 // TODO: change to welldone account!
 export default {
-  auth0Domain: 'yoavs.eu.auth0.com',
-  authClientId: '3uc9MmdkPEwLRf6KFC8TDN356TX6Kqkj',
+  auth0Domain: 'tabonoyo.eu.auth0.com',
+  authClientId: 'jdk3F2EstPkViJp2TJskg1oN4H7CHGev',
 }
